6de7c76103246413ccb46bf3d2c008704e1de4c4
Priscilla Joly <priscilla.joly@epitech.eu>
2020-10-08 22:40:07 +0200

    last commit

--- src/models/pawn.js
+++ src/models/pawn.js
@@ -1,23 +1,36 @@
 import EventManager from "../eventManager";
 
 export default class Pawn extends EventManager {
-    getLife(){
+    constructor(life, strength, def){
+        super()
 
+        this.life = life;
+        this.strength = strength;
+        this.def = def;
+    }
+
+    getLife(){
+        return this.life
     }
 
     getStrength(){
-
+        return this.strength
     }
 
     getDef(){
-
+        return this.def
     }
 
-    attack(){
-
+    attack(target){
+        return target.receiveAttack(this)
     }
 
-    receiveAttack(){
-
+    receiveAttack(opponent, strikeBack= false){
+        if (strikeBack) {
+            this.life = this.life - opponent.def
+        } else {
+            this.life = this.life - opponent.strength
+            opponent.receiveAttack(this, true)
+        }
     }
 }
--- src/models/player.js
+++ src/models/player.js
@@ -1,17 +1,43 @@
+import Pawn from "./pawn";
 import ModelFactory from './factory';
 
-export default class Player {
+export default class Player extends Pawn {
 
     constructor(config) {
-        this.type = config.type;
+        super(config.life, config.strength, config.def)
 
-        this.deck = ModelFactory.get('deck');
+        this.type = config.type;
+        this.deck = config.deck;
+    }
+
+    suffle(type){
+        const arrayCopy = this.deck.slice()
+        let j, x, i
+
+        if(type === "deck"){
+            for (i = this.deck.length - 1; i > 0; i--) {
+                j = Math.floor(Math.random() * (i + 1));
+                x = this.deck[i];
+                this.deck[i] = this.deck[j];
+                this.deck[j] = x;
+            }
+            return JSON.stringify(this.deck) !== JSON.stringify(arrayCopy);
+        }
     }
 
     draw () {
         return this.deck.draw();
     }
 
+    playCard(position){
 
+    }
 
+    discard(position){
+
+    }
+
+    // discard(position, target){
+    //
+    // }
 }
